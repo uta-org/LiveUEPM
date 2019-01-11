8e1a896cbe9eefe6b6168df9c56eccb4c546eca4
Ikillnukes <ikillnukes1@gmail.com>
2019-01-11 04:32:45 +0100

    Initial commit

---
+++ .VSSolution/LiveUEPM.Tests/LiveUEPM.Tests/App/ProcessWatcher.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace LiveUEPM.Tests.App
+{
+    public class ProcessWatch : IDisposable
+    {
+        private bool disposed = false;
+        private bool isUnityTriggered;
+
+        private ManagementEventWatcher processStartEvent = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStartTrace");
+        private ManagementEventWatcher processStopEvent = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStopTrace");
+
+        public event Action<PropertyDataCollection> UnityInitialized = delegate { };
+
+        public event Action<PropertyDataCollection> UnityStopped = delegate { };
+
+        public bool Debug { get; }
+
+        private ProcessWatch()
+        {
+        }
+
+        public ProcessWatch(bool fDebug = false)
+        {
+            Debug = fDebug;
+            isUnityTriggered = Process.GetProcessesByName("UnityCrashHandler64").Length > 0;
+
+            processStartEvent.EventArrived += new EventArrivedEventHandler(processStartEvent_EventArrived);
+            processStartEvent.Start();
+            processStopEvent.EventArrived += new EventArrivedEventHandler(processStopEvent_EventArrived);
+            processStopEvent.Start();
+        }
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                processStartEvent.Stop();
+                processStartEvent.Dispose();
+
+                processStopEvent.Stop();
+                processStopEvent.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        ~ProcessWatch()
+        {
+            processStartEvent.Stop();
+        }
+
+        private void processStartEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            PropertyDataCollection props;
+            if (IsUnityProcess(e, out props, false))
+                UnityInitialized?.Invoke(props);
+
+            if (Debug)
+                DebugInfo(e, false);
+        }
+
+        private void processStopEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            PropertyDataCollection props;
+            if (IsUnityProcess(e, out props, true))
+                UnityInitialized?.Invoke(props);
+
+            if (Debug)
+                DebugInfo(e, true);
+        }
+
+        private bool IsUnityProcess(EventArrivedEventArgs e, out PropertyDataCollection props, bool fStopped)
+        {
+            props = e.NewEvent.Properties;
+            string processName = props["ProcessName"].Value.ToString();
+
+            if (processName == "Unity.exe")
+                isUnityTriggered = true;
+
+            if (isUnityTriggered && (!fStopped && processName.Contains("UnityCrashHandler") || !fStopped && processName == "UnityShaderCompiler.exe"))
+            {
+                if (fStopped)
+                    isUnityTriggered = false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void DebugInfo(EventArrivedEventArgs e, bool fStopped)
+        {
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            string processID = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value).ToString();
+
+            Console.WriteLine($"Process {(fStopped ? "stopped" : "started")}. Name: " + processName + " | ID: " + processID);
+        }
+    }
+}
---
+++ .VSSolution/LiveUEPM.Tests/LiveUEPM.Tests/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LiveUEPM.Tests
+{
+    using App;
+
+    internal class Program
+    {
+        public const string ProjectPathGroup = "ProjectPath";
+
+        private static string _openendProjectpath;
+        private static bool fOpenException;
+
+        public static string CurrentOpenedProjectPath
+        {
+            get
+            {
+                if (fOpenException)
+                    return string.Empty;
+
+                try
+                {
+                    if (string.IsNullOrEmpty(_openendProjectpath))
+                    {
+                        string contents = GetFileText(EditorLogPath);
+                        _openendProjectpath = new Regex($@"COMMAND LINE ARGUMENTS:.+?\n.+?\n(?<{ProjectPathGroup}>(.+?)\nUsing)").Matches(contents).Cast<Match>().First().Groups[ProjectPathGroup].Value;
+                        // $@",.+?,\+(?<{ProjectPathGroup}>(.+?/Assets))"""
+                    }
+
+                    return _openendProjectpath;
+                }
+                catch
+                {
+                    fOpenException = true;
+                    return string.Empty;
+                }
+            }
+        }
+
+        public static string ProyectName
+        {
+            get
+            {
+                return Path.GetFileName(CurrentOpenedProjectPath);
+            }
+        }
+
+        public static string EditorLogPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Unity", "Editor", "Editor.log");
+        private static ProcessWatch Watcher { get; }
+
+        static Program()
+        {
+            Watcher = new ProcessWatch(true);
+            Watcher.UnityInitialized += (p) =>
+            {
+                Console.WriteLine($"Project '{ProyectName}' was opened!");
+            };
+            Watcher.UnityInitialized += (p) =>
+            {
+                Console.WriteLine($"Project '{ProyectName}' was closed!");
+            };
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(EditorLogPath);
+
+            //using (var watcher = new ProcessWatch())
+            Console.Read();
+
+            Watcher?.Dispose();
+        }
+
+        private static string GetFileText(string path)
+        {
+            string text;
+
+            using (FileStream logFileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var logFileReader = new StreamReader(logFileStream))
+                text = logFileReader.ReadToEnd();
+
+            return text;
+        }
+    }
+}
